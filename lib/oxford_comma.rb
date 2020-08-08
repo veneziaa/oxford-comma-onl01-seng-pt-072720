4863783bf073518d73d179c4822729a9c1cd9ebd
Venice Vargas <venicevargas1@live.com>
2020-08-08 05:37:45 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,4 +2,12 @@ def oxford_comma(array)
 
 end
 
-case 
+case array.length
+when 1 
+  "#{array[0]}"
+  when 2 
+    array[0..1].join("and")
+  else
+    array[0...-1].join(",")<<",and #{array[-1]}"
+  end
+end
